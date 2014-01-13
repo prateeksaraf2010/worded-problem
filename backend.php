fb2f14b5d0bc2fc02718bc643387dbd335b0f25d
Prateek Saraf <prateeksaraf2010@gmail.com>
2014-01-13 22:00:40 +0530

    Update backend.php

--- backend.php
+++ backend.php
@@ -1,226 +1,131 @@
 <?php
-$file=fopen("suggestions.txt","w+");
+$file=fopen("suggestions.txt","r+");
 	$statement=$_POST['statement'];
 	$class_name=$_POST['class_name'];
 	$fun_name=$_POST['fun_name'];
 	$no_of_args=$_POST['args'];
 	$return=$_POST['return'];
-
-	$add1c=$_POST['suggestfor1c'];
-	$add2c=$_POST['suggestfor2c'];
-	$add1j=$_POST['suggestfor1j'];
-	$add2j=$_POST['suggestfor2j'];
 $dim_ret=$_POST['dim_ret'];
-	$add1c="1-".$add1c;
-	$add2c="2-".$add2c;
-	
-
-		fwrite($file, $add1c);
-			fwrite($file,";");
-			//fwrite($file,$add1j);
-			//fwrite($file,"|");
-			fwrite($file, $add2c);
-			fwrite($file,";");
-			//fwrite($file,$add2j);
-			
-			
-  
-			$a="";
+			$a="";$temp1="";$temp2="";
 	$arg1_series="";$arg2_series="";
 	$argr_series="";
 	$arg1j_series="";$arg2j_series="";
 	$argrj_series="";
 	fseek($file,0);
+	// for dimension 1
+	  if(fgetc($file)=='1');
+	  
+  if(fgetc($file)=='-');
+  {
+
+  $a=fgetc($file);
+  while($a!=';'&&!feof($file))
+  {
+  
+   $temp1=$temp1.$a;
+  
+  $a=fgetc($file);
+  }
+  
+  }
+  
+  //for dimension 2
+	$a=fgetc($file);
+  while($a!='2')
+  $a=fgetc($file);
+  
+  
+  if(fgetc($file)=='-');
+  {
+
+  $a=fgetc($file);
+  while($a!=';'&&!feof($file))
+  {
+  
+
+  $temp2=$temp2.$a;
+   $a=fgetc($file);
+  }
+    }
+	fclose($file);
+
 	$arg1=$_POST['arg1'];
 	$type1=$_POST['type_arg1'];
 	$dim1=$_POST['dim1'];
+	$arg1_series="$type1"." "."$arg1";
+	$arg1j_series="$type1";
 	if($arg1!=null){
 		switch($dim1){
-			case 0:
-				$arg1_series="$type1 $arg1";
-				$arg1j_series="$type1 $arg1";
+			case 0:			
+				$arg1j_series=$arg1j_series." ".$arg1;
 				break;
 				
-			case 1:
-
-  if(fgetc($file)=='1');
-  if(fgetc($file)=='-');
-  {
-  $arg1_series="$type1";
-  $arg1j_series="$type1";
-  $arg1_series= $arg1_series." ".$arg1;
-  $a=fgetc($file);
-  while($a!=';'&&!feof($file))
-  {
-  
-
-  $arg1_series=$arg1_series.$a;
-  $arg1j_series=$arg1j_series.$a;
-  $a=fgetc($file);
-  }
-  
-  }
-    $arg1j_series= $arg1j_series." ".$arg1;
-  
- 		break;
+			case 1:			
+				$arg1_series=$arg1_series.$temp1;
+				 $arg1j_series=$arg1j_series.$temp1;
+				 $arg1j_series= $arg1j_series." ".$arg1;	
+				break;
 		
-			case 2:
-			
-  $a=fgetc($file);
-  while($a!='2')
-  $a=fgetc($file);
-  
-  
-  if(fgetc($file)=='-');
-  {
-  $arg1_series="$type1";
-  $arg1j_series="$type1";
-   $arg1_series=$arg1_series." ".$arg1;
-  $a=fgetc($file);
-  while($a!=';'&&!feof($file))
-  {
-  
-
-  $arg1_series=$arg1_series.$a;
-  $arg1j_series=$arg1j_series.$a;
-  
-  $a=fgetc($file);
-  }
- 
- 
-  }
- $arg1j_series=$arg1j_series." ".$arg1;
- 		break;
+			case 2:		
+				$arg1_series=$arg1_series.$temp2;
+				 $arg1j_series=$arg1j_series.$temp2;
+				 $arg1j_series= $arg1j_series." ".$arg1;
+				break;
 		
 	}}
-		
-	fseek($file,0);
+	
 	$arg2=$_POST['arg2'];
 	$type2=$_POST['type_arg2'];
 	$dim2=$_POST['dim2'];
+	$arg2_series="$type2"." "."$arg2";
+	$arg2j_series="$type2";
+	
 	if($arg2!=null){
 		switch($dim2){
-			case 0:
-				$arg2_series="$type2 $arg2";
-				$arg2j_series="$type2 $arg2";
+			case 0:				
+				$arg2j_series=$arg2j_series." ".$arg2;
 				break;
 				
-			case 1:
-
-  if(fgetc($file)=='1');
-  if(fgetc($file)=='-');
-  {
-  $arg2_series="$type2";
-  $arg2j_series="$type2";
-  $arg2_series= $arg2_series." ".$arg2;
-  $a=fgetc($file);
-  while($a!=';'&&!feof($file))
-  {
-  
-
-  $arg2_series=$arg2_series.$a;
-   $arg2j_series=$arg2j_series.$a;
-  
-  $a=fgetc($file);
-  }
-  $arg2j_series= $arg2j_series." ".$arg2;
-  }
-  
- 		break;
-			case 2:
-			
-  $a=fgetc($file);
-  while($a!='2')
-  $a=fgetc($file);
-  
-  
-  if(fgetc($file)=='-');
-  {
-  $arg2_series="$type2";
-    $arg2j_series="$type2";
-	$arg2_series=$arg2_series." ".$arg2;
-  $a=fgetc($file);
-  while($a!=';'&&!feof($file))
-  {
-  
-
-  $arg2_series=$arg2_series.$a;
-   $arg2j_series=$arg2j_series.$a;
-  
-  $a=fgetc($file);
-  }
-  $arg2j_series=$arg2j_series." ".$arg2;
- 
-  }
-
- 		break;
+			case 1:			
+				$arg2_series=$arg2_series.$temp1;
+				 $arg2j_series=$arg2j_series.$temp1;
+				 $arg2j_series= $arg2j_series." ".$arg1;
+				break;
+		
+			case 2:		
+				$arg2_series=$arg2_series.$temp2;
+				 $arg2j_series=$arg2j_series.$temp2;
+				 $arg2j_series= $arg2j_series." ".$arg1;
+				break;
 		
 	}}
 	
-		fseek($file,0);
+	$argr_series="$return" ;
+	
 if($return!=null){
 		switch($dim_ret){
-			case 0:
-				$argr_series="$return ";
-				break;
-				
 			case 1:
-
-  if(fgetc($file)=='1');
-  if(fgetc($file)=='-');
-  {
-  $argr_series="$return ";
-  $a=fgetc($file);
-  while($a!=';'&&!feof($file))
-  {
-  
-
-  $argr_series=$argr_series.$a;
-  
-  $a=fgetc($file);
-  }
-  $argr_series= $arg2_series." ";
-  }
-  
- 		break;
-			case 2:
-			
-  $a=fgetc($file);
-  while($a!='2')
-  $a=fgetc($file);
-  
-  
-  if(fgetc($file)=='-');
-  {
-  $argr_series="$return ";
-  $a=fgetc($file);
-  while($a!=';'&&!feof($file))
-  {
-  
-
-  $argr_series=$argr_series.$a;
-  
-  $a=fgetc($file);
-  }
-  $argr_series=$argr_series." ";
- 
-  }
-
- 		break;
+				$argr_series=$argr_series.$temp1." ";
+				 break;
 		
-	}}
+			case 2:
+			$argr_series=$argr_series.$temp2." ";
+			break;
+		
+	}
 	
-	
+	}
 	$arg_list="$arg1_series,$arg2_series";
 	$argj_list="$arg1j_series,$arg2j_series";
 	
 	$c_format="$argr_series$fun_name($arg_list){\n\n//&nbsp;&nbsp;&nbsp;&nbsp;Type your code here\n\n}";
 	$java_format="public class $class_name{\n\n&nbsp;&nbsp;public $return $fun_name($argj_list){\n\n&nbsp;&nbsp;//&nbsp;&nbsp;Type your code here\n\n&nbsp;&nbsp;}\n\n}//end of class";
-   fclose($file);
+   
 	?>
 <!doctype html>
 <html>
-	<head></head>
+	<head>
+</head>
 	<body>
 		<input type="hidden" id="c_code" value="<?php echo $c_format;?>">
 		<input type="hidden" id="java_code" value="<?php echo $java_format;?>">
